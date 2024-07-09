4f3725f42c538a69b7c327ca5a352ff7edcea65f
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2024-07-09 12:01:26 +0200

    nixos/tests/glitchtip: init

---
+++ nixos/tests/glitchtip.nix
@@ -0,0 +1,19 @@
+import ./make-test-python.nix ({ lib, ... }:
+
+{
+  name = "glitchtip";
+  meta.maintainers = with lib.maintainers; [ soyouzpanda ];
+
+  nodes.machine = { ... }:
+    {
+      services.glitchtip = {
+        enable = true;
+      };
+    };
+
+  testScript = ''
+    machine.wait_for_unit("glitchtip.service")
+    machine.wait_for_open_port(8080)
+    machine.succeed("curl --fail http://127.0.0.1:8080")
+  '';
+})
