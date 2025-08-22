d9027851b4a98c115ccd0460fb3b69aa2fe25413
Nico Felbinger <nico@felbinger.eu>
2025-08-22 19:06:01 +0200

    python3Packages.http-tools: init at 6.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7801,6 +7801,8 @@ self: super: with self; {
 
   http-snapshot = callPackage ../development/python-modules/http-snapshot { };
 
+  http-tools = callPackage ../development/python-modules/http-tools { };
+
   httpagentparser = callPackage ../development/python-modules/httpagentparser { };
 
   httpauth = callPackage ../development/python-modules/httpauth { };
